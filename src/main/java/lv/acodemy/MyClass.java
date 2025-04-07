a7b3154ad52a25aaf57f0c221c600bb14b1a2ccd
Vova <vladimirs.lazarevs1@gmail.com>
2025-04-07 20:20:16 +0300

    [feature/lesson_1]: Learn Java Variables

---
+++ src/main/java/lv/acodemy/MyClass.java
@@ -0,0 +1,41 @@
+package lv.acodemy;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Ja u Mami programmist!");
+        byte age = 30;
+        System.out.println(age);
+        short currentYear = 2025;
+        short temperature = -273;
+        short distance = 1600;
+        int score = 9000;
+        int population = 12347879;
+        int result = (10 * 10) - 42;
+        System.out.println(result);
+        long starsInGalaxy = 100_000_000_000L;
+        long bigNumber = 43923473532904756L;
+        long currentMillis = System.currentTimeMillis();
+        System.out.println(currentMillis);
+        float pi = 3.14f;
+        float pNumber = (float) Math.PI;
+        float discount = 25.5f;
+        double accountBalance = 999999.999995;
+        double squareRoot = Math.sqrt(2);
+        System.out.println(squareRoot);
+        double piDouble = Math.PI;
+        System.out.println(pi);
+        System.out.println(piDouble);
+        char initial = 'N';
+        char digit = '7';
+        char symbol = '#';
+        char smiley = '\u263A';
+        System.out.println(smiley);
+        boolean isJavaFun = true;
+        boolean isTired = true;
+        boolean passed = (40>60);
+        String myFullName = "Vladimirs Lazarevs";
+
+
+
+    }
+}
